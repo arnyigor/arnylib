3eb10999658bdd66a6680bc0fc4c6233ed375741
arnyigor <arnyigor@gmail.com>
2017-05-07 20:17:07 +0300

    database

--- src/main/java/pw/aristos/arnylib/database/DBObjects.java
+++ src/main/java/pw/aristos/arnylib/database/DBObjects.java
@@ -6,7 +6,11 @@ import android.database.Cursor;
 import java.util.ArrayList;
 
 public interface DBObjects<E> {
-    ArrayList<E> getObjList( Context context, String query,String orderby);
+    ArrayList<E> getObjList(Context context, String query, String orderby);
 
     ArrayList<E> getCursorObjs(Cursor cursor);
+
+    E getCursorObject(Cursor cursor, E obj);
+
+    E getObject(Context context, String query);
 }
