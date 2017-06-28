38e25b2222f5c6dd02e56b590677ec9d58b35e2b
i.sedoy <i.sedoy@aristos.pw>
2017-06-28 17:42:55 +0300

    network

--- src/main/java/com/arny/arnylib/network/AndroidNetworkService.java
+++ src/main/java/com/arny/arnylib/network/AndroidNetworkService.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.util.Log;
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.ANRequest;
+import com.androidnetworking.common.Method;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.androidnetworking.interfaces.StringRequestListener;
@@ -40,15 +41,39 @@ public class AndroidNetworkService extends IntentService {
 		AndroidNetworking.forceCancelAll();  // All the requests with the given tag will be cancelled , even if any percent threshold is set , it will be cancelled forcefully.
 	}
 
+	public static void apiBuildRequest(String url, final OnStringRequestResult result) {
+		apiBuildRequest(url, Method.GET, null, result);
+	}
+
+	public static void apiBuildRequest(String url, final OnJSONObjectResult result) {
+		apiBuildRequest(url, Method.GET, null, result);
+	}
+
+	public static void apiBuildRequest(String url, JSONObject params, final OnStringRequestResult result) {
+		apiBuildRequest(url, Method.GET, params, result);
+	}
+
+	public static void apiBuildRequest(String url, JSONObject params, final OnJSONObjectResult result) {
+		apiBuildRequest(url, Method.GET, params, result);
+	}
+
+	public static void apiBuildRequest(String url, int method, final OnStringRequestResult result) {
+		apiBuildRequest(url, method, null, result);
+	}
+
+	public static void apiBuildRequest(String url, int method, final OnJSONObjectResult result) {
+		apiBuildRequest(url, method, null, result);
+	}
+
 	public static void apiBuildRequest(String url, int method, JSONObject params, final OnStringRequestResult result) {
 		apiBuildRequest(url, method, params, null, result);
 	}
 
-public static void apiBuildRequest(String url, int method, JSONObject params, final OnJSONObjectResult result) {
+	public static void apiBuildRequest(String url, int method, JSONObject params, final OnJSONObjectResult result) {
 		apiBuildRequest(url, method, params, null, result);
 	}
 
-	public static void apiBuildRequest(String url, int method, JSONObject params, JSONObject headers, final OnJSONObjectResult result){
+	public static void apiBuildRequest(String url, int method, JSONObject params, JSONObject headers, final OnJSONObjectResult result) {
 		ANRequest.GetRequestBuilder getRequestBuilder = new ANRequest.GetRequestBuilder(url, method);
 		StringBuilder builder = new StringBuilder();
 		builder.append(" >> Api Request: ");
@@ -80,6 +105,7 @@ public static void apiBuildRequest(String url, int method, JSONObject params, fi
 				Log.i("api", " <<  Api Response success: " + response);
 				result.onSuccess(response);
 			}
+
 			@Override
 			public void onError(ANError error) {
 				Log.e("api", " <<  Api Response error: " + error.getErrorDetail());
@@ -88,7 +114,7 @@ public static void apiBuildRequest(String url, int method, JSONObject params, fi
 		});
 	}
 
-	public static void apiBuildRequest(String url, int method, JSONObject params, JSONObject headers, final OnStringRequestResult result){
+	public static void apiBuildRequest(String url, int method, JSONObject params, JSONObject headers, final OnStringRequestResult result) {
 		ANRequest.GetRequestBuilder getRequestBuilder = new ANRequest.GetRequestBuilder(url, method);
 		StringBuilder builder = new StringBuilder();
 		builder.append(" >> Api Request: ");
