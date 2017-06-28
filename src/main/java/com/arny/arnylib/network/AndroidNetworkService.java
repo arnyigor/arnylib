d25434412bda6978e8d6bc9ee368a6f52e01aacc
i.sedoy <i.sedoy@aristos.pw>
2017-06-28 16:58:22 +0300

    network

--- src/main/java/com/arny/arnylib/network/AndroidNetworkService.java
+++ src/main/java/com/arny/arnylib/network/AndroidNetworkService.java
@@ -1,31 +1,18 @@
 package com.arny.arnylib.network;
 
 import android.app.IntentService;
-import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.util.Log;
-import com.android.volley.*;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.androidnetworking.AndroidNetworking;
-import com.androidnetworking.common.Priority;
+import com.androidnetworking.common.ANRequest;
 import com.androidnetworking.error.ANError;
-import com.androidnetworking.interfaces.JSONArrayRequestListener;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.androidnetworking.interfaces.StringRequestListener;
-import com.arny.arnylib.utils.Utility;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
-import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
 
 public class AndroidNetworkService extends IntentService {
 	public AndroidNetworkService() {
@@ -53,133 +40,79 @@ public class AndroidNetworkService extends IntentService {
 		AndroidNetworking.forceCancelAll();  // All the requests with the given tag will be cancelled , even if any percent threshold is set , it will be cancelled forcefully.
 	}
 
-	public static void apiGetRequest(String url, String tag, final OnJSONArrayResult requestResult) {
-		Log.i("api", " >> Api Request: " + url);
-		AndroidNetworking.get(url)
-				.setTag(tag)
-				.setPriority(Priority.LOW)
-				.build()
-				.getAsJSONArray(new JSONArrayRequestListener() {
-					@Override
-					public void onResponse(JSONArray response) {
-						Log.i("api", " <<  Api Response success: " + response);
-						requestResult.onSuccess(response);
-					}
-
-					@Override
-					public void onError(ANError anError) {
-						Log.e("api", " <<  Api Response error: " + anError.getErrorDetail());
-						requestResult.onError(anError.getErrorDetail());
-					}
-				});
+	public static void apiBuildRequest(String url, int method, JSONObject params, final OnStringRequestResult result) {
+		apiBuildRequest(url, method, params, null, result);
 	}
 
-	public static void apiGetRequest(String url, String tag, final OnStringRequestResult requestResult) {
-		Log.i("api", " >> Api Request: " + url);
-		AndroidNetworking.get(url)
-				.setTag(tag)
-				.setPriority(Priority.LOW)
-				.build()
-				.getAsString(new StringRequestListener() {
-					@Override
-					public void onResponse(String response) {
-						Log.i("api", " <<  Api Response success: " + response);
-						requestResult.onSuccess(response);
-					}
-
-					@Override
-					public void onError(ANError anError) {
-						Log.e("api", " <<  Api Response error: " + anError.getErrorDetail());
-						requestResult.onError(anError.getErrorDetail());
-					}
-				});
+	public static void apiBuildRequest(String url, int method, JSONObject params, JSONObject headers, final OnJSONObjectResult result){
+		ANRequest.GetRequestBuilder getRequestBuilder = new ANRequest.GetRequestBuilder(url, method);
+		StringBuilder builder = new StringBuilder();
+		builder.append(" >> Api Request: ");
+		builder.append(" url ");
+		builder.append(url);
+		if (headers != null) {
+			try {
+				getRequestBuilder.addHeaders(getJsonObjectToHashMap(headers));
+				builder.append(" headers ");
+				builder.append(headers);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		if (params != null) {
+			try {
+				getRequestBuilder.addPathParameter(getJsonObjectToHashMap(params));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		ANRequest anRequest = getRequestBuilder.build();
+		Log.i("api", " >> Api Request: " + url + " with params: " + params);
+		anRequest.getAsJSONObject(new JSONObjectRequestListener() {
+			@Override
+			public void onResponse(JSONObject response) {
+				Log.i("api", " <<  Api Response success: " + response);
+				result.onSuccess(response);
+			}
+			@Override
+			public void onError(ANError error) {
+				Log.e("api", " <<  Api Response error: " + error.getErrorDetail());
+				result.onError(error.getErrorDetail());
+			}
+		});
 	}
 
-	public static void apiGetRequest(String url, String tag, final OnJSONObjectResult result) {
-		Log.i("api", " >> Api Request: " + url);
-		AndroidNetworking.get(url)
-				.setTag(tag)
-				.setPriority(Priority.LOW)
-				.build()
-				.getAsJSONObject(new JSONObjectRequestListener() {
-					@Override
-					public void onResponse(JSONObject response) {
-						Log.i("api", " <<  Api Response success: " + response);
-						result.onSuccess(response);
-					}
+	public static void apiBuildRequest(String url, int method, JSONObject params, JSONObject headers, final OnStringRequestResult result){
+		ANRequest.GetRequestBuilder getRequestBuilder = new ANRequest.GetRequestBuilder(url, method);
+		if (headers != null) {
+			try {
+				getRequestBuilder.addHeaders(getJsonObjectToHashMap(headers));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		if (params != null) {
+			try {
+				getRequestBuilder.addPathParameter(getJsonObjectToHashMap(params));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		ANRequest anRequest = getRequestBuilder.build();
+		Log.i("api", " >> Api Request: " + url + " with params: " + params);
 
-					@Override
-					public void onError(ANError anError) {
-						Log.e("api", " <<  Api Response error: " + anError.getErrorDetail());
-						result.onError(anError.getErrorDetail());
-					}
-				});
+		anRequest.getAsString(new StringRequestListener() {
+			@Override
+			public void onResponse(String response) {
+				Log.i("api", " <<  Api Response success: " + response);
+				result.onSuccess(response);
+			}
+
+			@Override
+			public void onError(ANError anError) {
+				Log.e("api", " <<  Api Response error: " + anError.getErrorDetail());
+				result.onError(anError.getErrorDetail());
+			}
+		});
 	}
-
-	public static void apiPostRequest(String url, String tag, JSONObject params, final OnJSONObjectResult requestResult) {
-		Log.i("api", " >> Api Request: " + url + " with params: " + params.toString());
-		AndroidNetworking.post(url)
-				.addJSONObjectBody(params)
-				.setTag(tag)
-				.setPriority(Priority.MEDIUM)
-				.build()
-				.getAsJSONObject(new JSONObjectRequestListener() {
-					@Override
-					public void onResponse(JSONObject response) {
-						Log.i("api", " <<  Api Response success: " + response);
-						requestResult.onSuccess(response);
-					}
-
-					@Override
-					public void onError(ANError anError) {
-						Log.e("api", " <<  Api Response error: " + anError.getErrorDetail());
-						requestResult.onError(anError.getErrorDetail());
-					}
-				});
-	}
-
-	public static void apiPostRequest(String url, String tag, JSONObject params, final OnStringRequestResult requestResult) {
-		Log.i("api", " >> Api Request: " + url + " with params: " + params.toString());
-		AndroidNetworking.post(url)
-				.addJSONObjectBody(params)
-				.setTag(tag)
-				.setPriority(Priority.MEDIUM)
-				.build()
-				.getAsString(new StringRequestListener() {
-					@Override
-					public void onResponse(String response) {
-						Log.i("api", " <<  Api Response success: " + response);
-						requestResult.onSuccess(response);
-					}
-
-					@Override
-					public void onError(ANError anError) {
-						Log.e("api", " <<  Api Response error: " + anError.getErrorDetail());
-						requestResult.onError(anError.getErrorDetail());
-					}
-				});
-	}
-
-	public static void apiPostRequest(String url, String tag, JSONObject params, final OnJSONArrayResult arrayResult) {
-		Log.i("api", " >> Api Request: " + url + " with params: " + params.toString());
-		AndroidNetworking.post(url)
-				.addJSONObjectBody(params)
-				.setTag(tag)
-				.setPriority(Priority.MEDIUM)
-				.build()
-				.getAsJSONArray(new JSONArrayRequestListener() {
-					@Override
-					public void onResponse(JSONArray response) {
-						Log.i("api", " <<  Api Response success: " + response);
-						arrayResult.onSuccess(response);
-					}
-
-					@Override
-					public void onError(ANError anError) {
-						Log.e("api", " <<  Api Response error: " + anError.getErrorDetail());
-						arrayResult.onError(anError.getErrorDetail());
-					}
-				});
-	}
-
 }
