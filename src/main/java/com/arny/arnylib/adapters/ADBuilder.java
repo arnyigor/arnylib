309ad73446c6d1999a903b3cad323facc009fc78
arny <arnyigor@gmail.com>
2017-10-04 22:30:36 +0300

    dialog

---
+++ src/main/java/com/arny/arnylib/adapters/ADBuilder.java
@@ -0,0 +1,35 @@
+package com.arny.arnylib.adapters;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+public abstract class ADBuilder extends AlertDialog.Builder {
+	protected AlertDialog mAlertDialog;
+	protected View view;
+
+	public ADBuilder(Context context, int themeResId) {
+		super(context, themeResId);
+		view = LayoutInflater.from(getContext()).inflate(themeResId, null);
+	}
+
+	public ADBuilder(Context context) {
+		super(context);
+	}
+
+	@Override
+	public AlertDialog show() {
+		if (view == null) {
+			view = getView();
+		}
+		this.setView(view);
+		this.setTitle(getTitle());
+		initUI(view);
+		mAlertDialog = super.show();
+		return mAlertDialog;
+	}
+
+	protected abstract void initUI(View view);
+	protected abstract String getTitle();
+	protected abstract View getView();
+}
