737758a053165aaa9b0657a73faf096d1eed9e08
arny <arnyigor@gmail.com>
2018-01-18 00:04:37 +0300

    rx test

--- src/test/java/com/arny/arnylib/LibUnitTest.java
+++ src/test/java/com/arny/arnylib/LibUnitTest.java
@@ -1,17 +1,27 @@
 package com.arny.arnylib;
 
+import android.util.Log;
 import com.arny.arnylib.utils.Params;
 import com.arny.arnylib.utils.Stopwatch;
 import com.arny.arnylib.utils.Utility;
 import io.reactivex.Observable;
 import io.reactivex.android.plugins.RxAndroidPlugins;
+import io.reactivex.observers.TestObserver;
 import io.reactivex.plugins.RxJavaPlugins;
 import io.reactivex.schedulers.Schedulers;
 import org.junit.Before;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 import static junit.framework.Assert.assertTrue;
@@ -23,76 +33,91 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class LibUnitTest {
-//
-//    @ClassRule
-//    public static final RxImmediateSchedulerRule schedulers = new RxImmediateSchedulerRule();
-//
-//    @BeforeClass
-//    public static void setUpRxSchedulers() {
-//        Scheduler immediate = new Scheduler() {
-//            @Override
-//            public Disposable scheduleDirect(@NonNull Runnable run, long delay, @NonNull TimeUnit unit) {
-//                // this prevents StackOverflowErrors when scheduling with a delay
-//                return super.scheduleDirect(run, 0, unit);
-//            }
-//
-//            @Override
-//            public Worker createWorker() {
-//                return new ExecutorScheduler.ExecutorWorker(Runnable::run);
-//            }
-//        };
-//
-//        RxJavaPlugins.setInitIoSchedulerHandler(scheduler -> immediate);
-//        RxJavaPlugins.setInitComputationSchedulerHandler(scheduler -> immediate);
-//        RxJavaPlugins.setInitNewThreadSchedulerHandler(scheduler -> immediate);
-//        RxJavaPlugins.setInitSingleSchedulerHandler(scheduler -> immediate);
-//        RxAndroidPlugins.setInitMainThreadSchedulerHandler(scheduler -> immediate);
-//    }
 
-    @Before
-    public void init() throws Exception {
-        // override Schedulers.io()
-        RxJavaPlugins.setIoSchedulerHandler(schedulerCallable -> Schedulers.trampoline());
-        // override AndroidSchedulers.mainThread()
-        RxAndroidPlugins.setInitMainThreadSchedulerHandler(schedulerCallable -> Schedulers.trampoline());
-    }
+	private Stopwatch stopwatch;
+	private static class ImmediateSchedulersRule implements TestRule {
+		@Override
+		public Statement apply(final Statement base, Description description) {
+			return new Statement() {
+				@Override
+				public void evaluate() throws Throwable {
+					RxJavaPlugins.setIoSchedulerHandler(scheduler -> Schedulers.trampoline());
+					RxJavaPlugins.setComputationSchedulerHandler(scheduler -> Schedulers.trampoline());
+					RxJavaPlugins.setNewThreadSchedulerHandler(scheduler -> Schedulers.trampoline());
 
-    @Test
-    public void rx() throws Exception {
-        Stopwatch stopwatch = new Stopwatch();
-        stopwatch.start();
-        System.out.println("rx: start:" + Utility.getThread() + " time:" + stopwatch.getElapsedTimeSecs(3));
-        Utility.mainThreadObservable(Observable.fromCallable(() -> {
-            System.out.println("rx: in:" + Utility.getThread() + " time:" + stopwatch.getElapsedTimeSecs(3));
-            return 0;
-        })).delay(10, TimeUnit.SECONDS).subscribe(integer -> {
-            System.out.println("rx: end in thread:" + Utility.getThread() + " time:" + stopwatch.getElapsedTimeSecs(3));
-        }, Throwable::printStackTrace);
-    }
+					try {
+						base.evaluate();
+					} finally {
+						RxJavaPlugins.reset();
+					}
+				}
+			};
+		}
+	}
 
-    //    @Test
-    public void emails_isCorrect() throws Exception {
-        List<String> list = Arrays.asList("","4844","@","a@m.r","a@.ru");
-        for (String s : list) {
-            assertTrue(!Utility.isEmailValid(s));
-        }
-    }
+	@Rule
+	public final ImmediateSchedulersRule schedulers = new ImmediateSchedulersRule();
 
-    //    @Test
-    public void test_is_empty() throws Exception {
-        assertTrue(Utility.empty(""));
-        assertTrue(Utility.empty(null));
-        assertTrue(Utility.empty("null"));
-    }
+	@Test
+	public void testUsingImmediateSchedulersRule() {
+		// given:
+		stopwatch = new Stopwatch();
+		stopwatch.start();
+		TestObserver<String> observer = new TestObserver<>();
+		System.out.println("rx: start:" + Utility.getThread() + " time:" + stopwatch.getElapsedTimeSecs(3));
+		Observable.fromCallable(() -> {
+			System.out.println("rx: fromCallable:" + Utility.getThread() + " time:" + stopwatch.getElapsedTimeSecs(3));
+			return londTimeFunctionString();
+		}).subscribeOn(Schedulers.computation()).subscribe(observer);
+		// then:
+		observer.assertComplete();
+		observer.assertNoErrors();
+		System.out.println("rx: end:" + Utility.getThread() + " time:" + stopwatch.getElapsedTimeSecs(3));
+		stopwatch.stop();
+	}
 
-    //    @Test
-    public void params() throws Exception {
-        Params params = new Params("{}");
-        assertThat(!Utility.empty(params));
-        assertThat(params.toString()).isNull();
-        Params params1 = new Params("param1=1");
-        assertThat(params1).isNotNull();
-    }
+	private String londTimeFunctionString() throws Exception {
+		Stopwatch stopwatch = new Stopwatch();
+		stopwatch.start();
+		System.out.println( "londTimeFunctionString: started:");
+		try {
+			for (int i = 0; i < 5; i++) {
+				Thread.sleep(1000);
+				System.out.println( "londTimeFunctionString update in thread " + Utility.getThread() + ": time:" + stopwatch.getElapsedTimeSecs(3) + " sec");
+				if (i == 3) {
+					throw new Exception("Ops");
+				}
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println( "londTimeFunctionString: finished:" + stopwatch.getElapsedTimeSecs(3) + " sec");
+		stopwatch.stop();
+		return "long_time_function_result";
+	}
 
+	//    @Test
+	public void emails_isCorrect() throws Exception {
+		List<String> list = Arrays.asList("", "4844", "@", "a@m.r", "a@.ru");
+		for (String s : list) {
+			assertTrue(!Utility.isEmailValid(s));
+		}
+	}
+
+	//    @Test
+	public void test_is_empty() throws Exception {
+		assertTrue(Utility.empty(""));
+		assertTrue(Utility.empty(null));
+		assertTrue(Utility.empty("null"));
+	}
+
+	//    @Test
+	public void params() throws Exception {
+		Params params = new Params("{}");
+		assertThat(!Utility.empty(params));
+		assertThat(params.toString()).isNull();
+		Params params1 = new Params("param1=1");
+		assertThat(params1).isNotNull();
+	}
 
 }
