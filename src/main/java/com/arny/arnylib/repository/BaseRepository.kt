45e7abb25eab807a175fa8623fffbac6517f3760
i.sedoy <i.sedoy@aristos.pw>
2018-05-08 12:36:23 +0300

    refactor context,imports

--- src/main/java/com/arny/arnylib/repository/BaseRepository.kt
+++ src/main/java/com/arny/arnylib/repository/BaseRepository.kt
@@ -1,38 +1,36 @@
 package com.arny.arnylib.repository
 
-import android.content.Context
 import com.arny.arnylib.utils.Config
 
 abstract class BaseRepository : BaseRepositoryContract {
-    abstract fun getContext(): Context?
     override fun getString(resId: Int): String? {
-        return getContext()?.getString(resId)
+        return getContext().getString(resId)
     }
 
     override fun getPrefInt(key: String): Int? {
-        return getContext()?.let { Config.getInt(key, it) }
+        return getContext().let { Config.getInt(key, it) }
     }
 
     override fun getPrefString(key: String, default: String?): String? {
         if (default != null) {
-            return getContext()?.let { Config.getString(key, it, default) }
+            return getContext().let { Config.getString(key, it, default) }
         }
-        return getContext()?.let { Config.getString(key, it) }
+        return getContext().let { Config.getString(key, it) }
     }
 
     override fun setPrefString(key: String, value: String?) {
-        getContext()?.let { Config.setString(key, value, it) }
+        getContext().let { Config.setString(key, value, it) }
     }
 
     override fun setPrefBoolean(key: String, value: Boolean) {
-        getContext()?.let { Config.setBoolean(key, value, it) }
+        getContext().let { Config.setBoolean(key, value, it) }
     }
 
     override fun getPrefBoolean(key: String, default: Boolean): Boolean {
-        return getContext()?.let { Config.getBoolean(key, default, it) } ?: default
+        return getContext().let { Config.getBoolean(key, default, it) } ?: default
     }
 
     override fun removePref(key: String) {
-        return getContext()?.let { Config.remove(key, it) } ?: Unit
+        return getContext().let { Config.remove(key, it) } ?: Unit
     }
 }
--- src/main/java/com/arny/arnylib/repository/BaseRepositoryContract.kt
+++ src/main/java/com/arny/arnylib/repository/BaseRepositoryContract.kt
@@ -1,8 +1,10 @@
 package com.arny.arnylib.repository
 
+import android.content.Context
 import android.support.annotation.StringRes
 
 interface BaseRepositoryContract {
+    fun getContext(): Context
     fun getString(@StringRes resId: Int): String?
     fun getPrefString(key: String, default: String? = null): String?
     fun getPrefInt(key: String): Int?
